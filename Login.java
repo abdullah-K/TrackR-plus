3c250150170f0074ee274889c2fbbedf800fd715
mathew-luong <47367640+mathew-luong@users.noreply.github.com>
2019-04-07 16:39:43 -0600

    Update Login
    Update login to include json implementation

--- Login.java
+++ Login.java
@@ -7,40 +7,40 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 /**
-*the Login class is only called upon if it's the user's first time using the app
-*/
+ * the Login class is only called upon if it's the user's first time using the
+ * app
+ */
 
 public class Login {
-  
+
   private JSONObject userData;
 
-  public void userLoginGUI(String nameInput, double balanceInput){
+  public void userLoginGUI(String nameInput, double balanceInput, double goal) {
     try {
-        // make new User file (in the root directory)
-        File userFile = new File("user.json");
-        // create user JSON object
-        userData = new JSONObject();
-        // Main main = Main.getMainObject();
-        userData.put("username", nameInput);
-        userData.put("userBalance", balanceInput);
-        userData.put("savingsGoal", 0.0);
+      goal = 0; 
+      // make new User file (in the root directory)
+      File userFile = new File("user.json");
+      // create user JSON object
+      userData = new JSONObject();
+      userData.put("userName", nameInput);
+      userData.put("userBalance", balanceInput);
+      userData.put("savingsGoal", goal);
 
-        // write data (after converting it to a String) to the userFile with fileWriter
-        // reference: https://www.tutorialspoint.com/java/io/file_getabsolutefile.htm
-        FileWriter fileWriter = new FileWriter(userFile.getAbsoluteFile());
-        String jsonText = userData.toString();
-        fileWriter.write(jsonText);
-        // close the fileWriter to release the system resources being used and to prevent potential file corruption
-        fileWriter.close();
-      }
-      // print error stack traces for java.io exceptions
-      catch (FileNotFoundException error) {
-        error.printStackTrace();
-      }
-      catch (IOException error) {
-        error.printStackTrace();
-      }
+      // write data (after converting it to a String) to the userFile with fileWriter
+      // reference: https://www.tutorialspoint.com/java/io/file_getabsolutefile.htm
+      FileWriter fileWriter = new FileWriter(userFile.getAbsoluteFile());
+      String jsonText = userData.toString();
+      fileWriter.write(jsonText);
+      // close the fileWriter to release the system resources being used and to
+      // prevent potential file corruption
+      fileWriter.close();
+    }
+    // print error stack traces for java.io exceptions
+    catch (FileNotFoundException error) {
+      error.printStackTrace();
+    } catch (IOException error) {
+      error.printStackTrace();
+    }
   }
-  
-}
 
+}
