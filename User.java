3a4050ad90aebd6771572d9d7015284d65ac5a9f
samwambam <47361269+samwambam@users.noreply.github.com>
2019-04-08 10:27:05 -0600

    Update User.java
    Worked on the commenting

--- User.java
+++ User.java
@@ -9,12 +9,18 @@ public class User {
   private double userBalance = 0.0;
   private JSONObject userData;
 
+  /**
+  *default constructor 
+  */
   public User() {
     setUserBalance(0.0);
     setUserName("");
     setSavingsGoal(0.0);
   }
 
+  /**
+  *constructor that takes in the string of the filename 
+  */
   public User(String filename) {
     try {
       String jsonContent = new Scanner(new File(filename)).useDelimiter("\\Z").next();
@@ -28,39 +34,62 @@ public class User {
     }
   }
 
+  /**
+  * constructor that takes in the name, balance and goal
+  */
   public User(String name, double balance, double goal){
     setUserName(name);
     setUserBalance(balance);
     setSavingsGoal(goal);
   }
 
+  /**
+  * returns the JSONObject with data 
+  */
   public JSONObject getUserData(){
     return this.userData;
   }
 
+  /**
+  * returns the name of the user 
+  */
   public String getUserName() {
     return this.userName;
   }
 
+  /**
+  * returns the savings goal of the user 
+  */
   public double getSavingsGoal() {
     return this.savingsGoal;
   }
 
+  /**
+  * returns the balance of the user 
+  */
   public double getUserBalance() {
     return this.userBalance;
   }
 
+  /**
+  * sets the name of the user 
+  */
   public void setUserName(String userName) {
     this.userName = userName;
   }
 
+  /**
+  * sets the savings goal of the user 
+  */
   public void setSavingsGoal(double savingsGoal) {
     // add non-negative/non-weird-character condition here
     this.savingsGoal = savingsGoal;
   }
 
+  /**
+  * sets the balance of the user 
+  */
   public void setUserBalance(double userBalance) {
     this.userBalance = userBalance;
   }
-
 }
